325e2e9a0b3ce3d5d1f0aec5fa02f59260fcf7d2
Vince <starryblue7@gmail.com>
2021-10-20 11:07:21 -0700

    add get/post & delete routes for notes

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const path = require('path');
+const uuid = require('uuid');
+const fs = require('fs');
+
+const db = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
